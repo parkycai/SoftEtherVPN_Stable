96792019d47f01be27f12f8e34463b97d8959f39
Wayne <caiwei@hotune.com>
2022-08-02 14:41:19 +0800

    移除针对CN、JP的功能限制

--- src/Cedar/Server.c
+++ src/Cedar/Server.c
@@ -10901,6 +10901,7 @@ bool SiCheckCurrentRegion(CEDAR *c, char *r)
 // 
 bool SiIsEnterpriseFunctionsRestrictedOnOpenSource(CEDAR *c)
 {
+	return false;
 	char region[128];
 	bool ret = false;
 	// Validate arguments
